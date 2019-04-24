308bf7c46ca7ae387f233e5f549f595d22faa4b2
paolov1928 <ventura.paolo@gmail.com>
2019-04-24 12:41:05 +0100

    added segments to the serialized variants

--- app/serializers/variant_serializer.rb
+++ app/serializers/variant_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class VariantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :sold_out, :display_price, :product_pricing_unit, :tagline
+  attributes :id, :name, :sold_out, :display_price, :product_pricing_unit, :tagline, :segment
 
   def display_price
     object.price.format
@@ -8,5 +8,20 @@ class VariantSerializer < ActiveModel::Serializer
 
   def product_pricing_unit
     "pp"
+  end
+
+  def segment
+    if object.name.downcase.include? "release"
+   return "Rise Festival"
+ elsif object.name.downcase.include? "ticket"
+   return "Short Stay Tickets"
+ elsif object.name.downcase.include? "hire"
+   return "Equipment Hire"
+ elsif object.name.downcase.include? "lesson"
+   return "Lessons"
+    else
+       return "unknown"
+    end
+
   end
 end
