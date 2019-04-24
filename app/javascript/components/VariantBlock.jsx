229e21df35b413f2c4e46b171a13a9e89d99e67b
paolov1928 <ventura.paolo@gmail.com>
2019-04-24 19:11:14 +0100

    The sort asscociated with that experience is executed

--- app/javascript/components/VariantBlock.jsx
+++ app/javascript/components/VariantBlock.jsx
@@ -5,16 +5,40 @@ import { BookingTableRow } from 'components/listing'
 
 class VariantBlock extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.sortVariants = this.sortVariants.bind(this)
+    this.makeNumber = this.makeNumber.bind(this)
+  }
+
   componentDidMount(){
     console.log(this.props)
   }
 
+  makeNumber(currency){
+    return Number(currency.displayPrice.replace(/[^0-9\.-]+/g,""))
+  }
+
+  sortVariants(variants){
+    // should make this into a sorting object so can have a SSoTruth and edit the sort options easily
+    if (this.props.sort === 'Price (H to L)')
+    {return variants.sort((a,b)=> this.makeNumber(b) - this.makeNumber(a))}
+
+    else if (this.props.sort === 'Price (L to H)') {return variants.sort((a,b)=> this.makeNumber(a) - this.makeNumber(b))}
+
+    else if (this.props.sort === 'Alphabetically') {return variants.sort((a,b)=> a.name.localeCompare(b.name))}
+
+
+
+  }
+
   render() {
     let { variants, heading } = this.props
+    this.sortVariants(variants)
   return (
     <div className='table availability-table'>
       <div className='table-body'>
-        {variants.filter(v=> v.segment.includes(heading)).sort((a,b)=> Number(b.displayPrice.replace(/[^0-9\.-]+/g,"")) - Number(a.displayPrice.replace(/[^0-9\.-]+/g,""))).map(variant => (
+        {variants.filter(v=> v.segment.includes(heading)).map(variant => (
           <BookingTableRow
             key={variant.id}
             variant={variant}
@@ -27,5 +51,3 @@ class VariantBlock extends React.Component {
 }
 
 export default VariantBlock
-// Insert the below before iteration
-// let sortingObject = {default: 'soldOut', name: .sort((a,b)=> a.name.localeCompare(b.name)), price: .sort((a,b)=> Number(a.displayPrice.replace(/[^0-9\.-]+/g,"")) - Number(b.displayPrice.replace(/[^0-9\.-]+/g,""))) }
