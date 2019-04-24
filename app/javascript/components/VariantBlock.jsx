7aa44700f8082b571acac3a41e7e7018e309db44
paolov1928 <ventura.paolo@gmail.com>
2019-04-24 14:09:24 +0100

    tried to add in sortable list but it mucks up other compatibility

--- app/javascript/components/VariantBlock.jsx
+++ app/javascript/components/VariantBlock.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import { BookingTableRow } from 'components/listing'
+import {sortableContainer, sortableElement} from 'react-sortable-hoc';
+import arrayMove from 'array-move';
 
+const SortableItem = sortableElement(({value}) => <li>{value}</li>);
+
+const SortableContainer = sortableContainer(({children}) => {
+  return <ul>{children}</ul>;
+});
 
 
 class VariantBlock extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {variants: this.props.variants}
+    this.onSortEnd = this.onSortEnd.bind(this);
+  }
+
+  onSortEnd({oldIndex, newIndex}){
+    this.setState(({variants}) => ({
+      variants: arrayMove(variants, oldIndex, newIndex),
+    }));
+  };
 
   componentDidMount(){
     console.log(this.props)
@@ -14,12 +32,14 @@ class VariantBlock extends React.Component {
   return (
     <div className='table availability-table'>
       <div className='table-body'>
+        <SortableContainer onSortEnd={this.onSortEnd}>
         {variants.filter(a=> a.segment.includes(heading)).map(variant => (
           <BookingTableRow
             key={variant.id}
             variant={variant}
           />
         ))}
+        </SortableContainer>
       </div>
     </div>
   )
